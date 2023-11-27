c4f0e9dfdecd96f8781965d251591cf5abd96438
chen sisi <75115973+Bdeparture@users.noreply.github.com>
2023-11-27 20:33:14 +0800

    optimize the page

--- src/components/peopleList/index.js
+++ src/components/peopleList/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import People from "../peopleCard";
 import Grid from "@mui/material/Grid";
 
-const PeopleList = ( {people, action }) => {
+const PeopleList = ({ people, action }) => {
   //console.log(people)
   let peopleCards = people.map((p) => (
-    <Grid key={p.id} item xs={4} sm={4} md={3} lg={3} xl={3}>
+    <Grid key={p.id} item xs={12} sm={6} md={3} lg={3} xl={2}>
       <People people={p} action={action} />
     </Grid>
   ));
